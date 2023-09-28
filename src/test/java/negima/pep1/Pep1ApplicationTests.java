f723f8db21cc6901ab65c867c6554283b8cd3cf0
Negima <paolo.demarchi@usach.cl>
2023-09-27 21:28:25 -0300

    Project init

---
+++ src/test/java/negima/pep1/Pep1ApplicationTests.java
@@ -0,0 +1,13 @@
+package negima.pep1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pep1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
